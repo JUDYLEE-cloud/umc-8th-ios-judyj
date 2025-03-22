37eb2ea05b597d984a8698a3bf9e642d54ca2f49
JUDYLEE <ljh.social@ewhain.net>
2025-03-22 15:44:50 +0900

    Initial Commit

---
+++ 8thWorkbook/_thWorkbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  _thWorkbookApp.swift
+//  8thWorkbook
+//
+//  Created by 이주현 on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct _thWorkbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 8thWorkbookTests/_thWorkbookTests.swift
@@ -0,0 +1,17 @@
+//
+//  _thWorkbookTests.swift
+//  8thWorkbookTests
+//
+//  Created by 이주현 on 3/22/25.
+//
+
+import Testing
+@testable import _thWorkbook
+
+struct _thWorkbookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
