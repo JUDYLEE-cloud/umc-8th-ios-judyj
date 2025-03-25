30a3de6f38c2b6c50afa0e7458e346d414f0e8e6
JUDYLEE <ljh.social@ewhain.net>
2025-03-25 21:09:58 +0900

    1week Grammar

--- Grammar/FirstWeek.playground/Contents.swift
+++ Grammar/FirstWeek.playground/Contents.swift
@@ -1,13 +1,16 @@
 // 변수
 var name: String = "JudyJ"
 var age: Int = 20
+var dogage: Int = 2
 
 name = "leejuhyun"
 age = 23
+dogage = 3
 let ageBool = age > 20
 
 print("name: \(name)")
 print("age: \(age)")
+print("주인과 개의 나의를 곱하면 \(age * dogage)입니다")
 if ageBool {
     print("성인입니다")
 } else {
@@ -19,7 +22,7 @@ let pi: Double = 3.1434343
 let birthYear: Int = 1999
 print("pi: \(pi)")
 
-// 타임 추론
+// 타임 추론 type(of: 00)
 var number = 42
 var message = "Hello, world"
 var isAvailable = true
@@ -32,28 +35,28 @@ let intValue: Int = 30
 let doubleValue: Double = Double(intValue)
 let stringValue: String = String(intValue)
 
-// 빈 문자열
+// 빈 문자열 " "
 let emptyStringA: String = " "
 let emptyStringB: String = String()
 
-// 문자열 연결
+// 문자열 연결 +
 let firstName: String = "Lee"
 let lastName: String = "Juhyun"
 let fullName: String = firstName + emptyStringA + lastName
 print(fullName)
 
-// 문자열 길이 확인
+// 문자열 길이 확인 \(00.count)
 let messageNumber: String = "Hello, world!"
 print("Hello, world!의 알파벳 개수: \(messageNumber.count)")
 
-// 문자열 비교
+// 문자열 비교 if A == B
 let strA = "Hello"
 let strB = "Hello"
 if strA == strB {
     print("strA와 strB의 두 문자열 같음")
 }
 
-// 다중 문자열
+// 다중 문자열 """
 let longMessage = """
  첫번째줄은 이렇게 쓰고
  두번째줄을 이렇게 쓰고
@@ -61,6 +64,19 @@ let longMessage = """
 """
 print(longMessage)
 
-// 문자열 포함 여부 확인
+// 문자열 포함 여부 확인 00.contains("00")
 let profile = "hello my name is juhyun lee"
-print("Does profile contains your name? \sentence.contains("juhyun")")
+print("Does profile contains your name? \(profile.contains("juhyun"))")
+
+// 문자열이 특정 문자열로 시작 또는 끝나는지 확인하는 hasPrefix, hasSuffix
+let urlString: String = "www.naver.com"
+if urlString.hasPrefix("www") {
+    print("이 urlString은 www로 시작합니다")
+} else {
+    print("이 urlString은 www로 시작하지 않습니다")
+}
+if urlString.hasSuffix(".com") {
+    print("이 urlString은 .com으로 끝납니다")
+} else {
+    print("이 urlString은 www로 끝나지 않습니다")
+}
---
+++ Grammar/FirstWeekSilsoup.playground/Contents.swift
@@ -0,0 +1,13 @@
+// 문제 1 이름 상수, 나이 변수, 문자열 보간법
+let name: String = "JudyJ"
+var age: Int = 20
+
+print("이름 : \(name), 나이: \(age)")
+//print("이름: \(name)")
+//print("나이: \(age)")
+//
+//// 문제 2
+//let numA: Int = 10
+//let numB: Double = 10.2
+//let sum = numA + numA
+//print(sum)
